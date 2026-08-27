ce868bf38cb5f66d24aed151f265d31664d4130e
Leticia Abreu <xabreu.leticia@gmail.com>
2026-08-27 12:01:11 -0300

    Add files via upload

---
+++ clima/clima/src/main/java/com/example/clima/ClimaApplication.java
@@ -0,0 +1,13 @@
+package com.example.clima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaApplication.class, args);
+	}
+
+}
---
+++ clima/clima/src/test/java/com/example/clima/ClimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
